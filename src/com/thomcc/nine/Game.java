e6f3a07a2e441a0fd63bc335d7c184c2db1fbf4d
thom <chiovolonit@gmail.com>
2012-01-20 00:52:10 -0500

    momentum works but sucks to use

--- src/com/thomcc/nine/Game.java
+++ src/com/thomcc/nine/Game.java
@@ -7,21 +7,18 @@ public class Game {
   private Level _level;
   private InputHandler _ih;
   public int offX, offY;
+  
   public Game(InputHandler ih) {
     offX = 0;
     offY = 0;
-    
     _ih = ih;
     _level = new Level(256, 256);
    
     _player = new Player();
     _level.addPlayer(_player);
   }
-  public void setOffset(int x, int y) {
-    offX = x;
-    offY = y;
-  }
   
+  public void setOffset(int x, int y) { offX = x; offY = y; }
   public void tick() {
     _player.tick(_ih.up, _ih.down, _ih.left, _ih.right);
     int mx = _ih.mouseX;
--- src/com/thomcc/nine/Player.java
+++ src/com/thomcc/nine/Player.java
@@ -10,8 +10,13 @@ public class Player {
   private Level _level;
   private double _dist;
   private int _startX, _startY;
+  private int _px, _py; // momentum
+  private static final int MOMENTUM_MAX = 6;
   public Player() {
-    _startX = _startY = x = y = -1;
+    _startX = _startY = -1;
+    x = y = -1;
+    _px = 0;
+    _py = 0;
     dir = 0;
   }
   public void lookAt(int px, int py) {
@@ -21,24 +26,66 @@ public class Player {
     return (((int) (dir / (Math.PI * 2) * 16 + 20.5)) & 15);
   }
   public void tick(boolean u, boolean d, boolean l, boolean r) {
-    int dx = 0, dy = 0;
+    //int dx = 0, dy = 0;
     
-    if (d) ++dy;
-    if (r) ++dx;
-    if (l) --dx;
-    if (u) --dy;
-    
-    move(dx, dy);
+    if (d) _py += 2;
+    if (r) _px += 2;
+    if (l) _px -= 2;
+    if (u) _py -= 2;
+    if (i % 60 == 0) {
+      System.out.format("_px: %s, _py: %s\n", _px, _py);
+    }
+    updatePosition();
+    if (_px < 0) ++_px;
+    else if (_px > 0) --_px;
+    else if (_px > MOMENTUM_MAX) _px = MOMENTUM_MAX;
+    if (_py < 0) ++_py;
+    else if (_py > 0) --_py;
+    else if (_py > MOMENTUM_MAX) _py = MOMENTUM_MAX;
+    if(i++ % 60 == 0) {
+      
+    }
+    updateDistance();
+  }
+  private int i = 0;
+  public void updatePosition() {
+    if (_px == 0 && _py == 0) return;
+    if (_py != 0) {
+      int dy = (_py < 0) ? -1 : 1;
+      int t = Math.abs(_py);
+      for (int iy = 0; iy < t; ++iy) {
+        if (canMove(0, dy)) {
+          y += dy;
+        }
+        else {
+          _py = 0;
+          break;
+        }
+      }      
+    }
+    if (_px != 0) {
+      int dx = (_px < 0) ? -1 : 1;
+      int t = Math.abs(_px);
+      for (int ix = 0; ix < t; ix++) {
+        if (canMove(dx,0)) {
+          //System.out.println("moving x");
+          x += dx;
+        } else {
+          _px = 0;
+          break;
+        }
+      }
+    }
     
   }
-  
+  /*
   private void move(int dx, int dy) {
     if (dx == 0 && dy == 0) return;
     // separate to allow sliding along walls
     if (dx != 0 && canMove(dx, 0)) x += dx; 
     if (dy != 0 && canMove(0, dy)) y += dy;
     updateDistance();
-  }
+  }*/
   private void updateDistance() {
     double deltax = (this.x / 16.0) - (_startX / 16.0);
     double deltay = (this.y / 16.0) - (_startY / 16.0);
