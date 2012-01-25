3c3245d5f819e15baaa1d81931c3c0e760481685
thom <chiovolonit@gmail.com>
2012-01-25 09:55:26 -0500

    added modifier keys to input handler for potential future use (at least with debugging)

--- src/com/thomcc/nine/Input.java
+++ src/com/thomcc/nine/Input.java
@@ -5,10 +5,11 @@ import java.awt.event.*;
 public class Input implements KeyListener, MouseListener, MouseMotionListener {
   public int mouseX, mouseY;
   public boolean mouseDown;
-  public boolean up, down, left, right, debug, select, fire;
-  
+  public boolean up, down, left, right, select, fire;
+  public boolean mod_ctrl, mod_shift, mod_alt; 
   public Input(Nine game) {
     mouseDown = up = down = left = right = fire = select = false;
+    mod_ctrl = mod_shift = mod_alt = false;
     mouseX = mouseY = -1;
     game.addMouseListener(this);
     game.addMouseMotionListener(this);
@@ -19,33 +20,27 @@ public class Input implements KeyListener, MouseListener, MouseMotionListener {
     switch (e.getKeyCode()) {
     case KeyEvent.VK_UP:
     case KeyEvent.VK_W:
-    case KeyEvent.VK_NUMPAD8:
-      up = pressed;
-      break;
+    case KeyEvent.VK_NUMPAD8: up = pressed; break;
+    
     case KeyEvent.VK_DOWN:
     case KeyEvent.VK_S:
-    case KeyEvent.VK_NUMPAD2:
-      down = pressed;
-      break;
+    case KeyEvent.VK_NUMPAD2: down = pressed; break;
+    
     case KeyEvent.VK_LEFT:
     case KeyEvent.VK_A:
-    case KeyEvent.VK_NUMPAD4:
-      left = pressed;
-      break;
+    case KeyEvent.VK_NUMPAD4: left = pressed; break;
+    
     case KeyEvent.VK_RIGHT:
     case KeyEvent.VK_D:
-    case KeyEvent.VK_NUMPAD6:
-      right = pressed;
-      break;
-    case KeyEvent.VK_SPACE:
-      fire = pressed;
+    case KeyEvent.VK_NUMPAD6: right = pressed; break;
+    
+    case KeyEvent.VK_SPACE: fire = pressed;
     case KeyEvent.VK_ENTER:
-    case KeyEvent.VK_NUMPAD0:
-      select = pressed;
-      break;
-    case KeyEvent.VK_ESCAPE:
-      debug = pressed;
-      break;
+    case KeyEvent.VK_NUMPAD0: select = pressed; break;
+    
+    case KeyEvent.VK_ALT: mod_alt = pressed; break;
+    case KeyEvent.VK_SHIFT: mod_shift = pressed; break;
+    case KeyEvent.VK_CONTROL: mod_ctrl = pressed; break;
     }
   }
   public void setMouse(int x, int y) { mouseX = x/2; mouseY = y/2; }
